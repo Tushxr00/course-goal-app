84a18a0042b6dd1b37134310c79529436fce854d
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-04-18 20:00:48 +0530

    dynamic styled-component

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 // import styledComponents from "styled-components";
-import  styled  from "styled-components";
+import styled from "styled-components";
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
@@ -12,37 +12,29 @@ import "./CourseInput.css";
 const FormControl = styled.div`
   margin: 0.5rem 0;
 
-& label {
-  font-weight: bold;
-  display: block;
-  margin-bottom: 0.5rem;
-}
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.invalid ? "red" : "black")};
+  }
 
-& input {
-  display: block;
-  width: 100%;
-  border: 1px solid #ccc;
-  font: inherit;
-  line-height: 1.5rem;
-  padding: 0 0.25rem;
-}
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
+    background: ${(props) => (props.invalid ? "#ffd7d7" : "transparent")};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
 
-& input:focus {
-  outline: none;
-  background: #fad0ec;
-  border-color: #8b005d;
-}
-
-&.invalid input {
-  border-color: red;
-  background: #ffd7d7;
-}
-
-&.invalid label {
-  color: red;
-}
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
 `;
-
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -66,7 +58,8 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl className={!isValid ?"invalid":""}>
+      {/* <FormControl className={!isValid ?"invalid":""}> */}
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </FormControl>
